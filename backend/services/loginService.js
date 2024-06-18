784ca9c4f2182afa0ae45a0b1d39380fe9746f67
pandreea14 <andreea.pinte4@gmail.com>
2024-06-18 13:58:26 +0300

    loginAdmin

--- backend/services/loginService.js
+++ backend/services/loginService.js
@@ -1,9 +1,9 @@
-const { query } = require('../utils/dbUtils');
-const bcrypt = require('bcrypt');
-
+const { query } = require("../utils/dbUtils");
+const bcrypt = require("bcrypt");
+const { generateToken } = require("../utils/jwtUtils");
 
 async function getUserByEmail(email) {
-  const sql = 'SELECT * FROM users WHERE email = ?';
+  const sql = "SELECT * FROM users WHERE email = ? AND role = 'user'";
   const params = [email];
   const results = await query(sql, params);
 
@@ -11,7 +11,19 @@ async function getUserByEmail(email) {
     return { error: "User not found. Please register first." };
   }
 
-  return results[0];//daca a gasit emailul returneaza primul obiect(user) gasit si singurul de altfel
+  return results[0]; //daca a gasit emailul returneaza primul obiect(user) gasit si singurul de altfel
+}
+
+async function getAdminByEmail(email) {
+  const sql = "SELECT * FROM users WHERE email = ? AND role = 'admin'";
+  const params = [email];
+  const results = await query(sql, params);
+
+  if (results.length === 0) {
+    return { error: "Admin not found." };
+  }
+
+  return results[0]; //daca a gasit emailul returneaza primul obiect(user) gasit si singurul de altfel
 }
 
 async function comparePasswords(inputPassword, storedPassword) {
@@ -26,24 +38,30 @@ async function comparePasswords(inputPassword, storedPassword) {
 
 async function loginUser(email, password) {
   try {
-    const user = await getUserByEmail(email);
-
+    let user = await getUserByEmail(email);
     if (user.error) {
-      return user;
+      user = await getAdminByEmail(email);
+      if (user.error) {
+        return { error: "User not found. Please register first." };
+      }
     }
 
     const passwordMatch = await comparePasswords(password, user.password);
-
     if (passwordMatch.error) {
       return passwordMatch;
     }
 
     //generate token upon successful login
-    return { message: 'Login successful' };
+    console.log("email: ", user.email);
+    console.log("role: ", user.role);
+    const token = generateToken({ email: user.email, role: user.role });
+    // return { token };
+    return { message: "User logged in!", token };
+
   } catch (error) {
-    console.error("An error occurred during the login: ", error);
-    return { error: "An error occurred during the login: " + error.message };
+    console.error("Error in login service: ", error);
+    return { error: "Error in login service: " + error.message };
   }
 }
 
-module.exports = { loginUser, getUserByEmail };
+module.exports = { loginUser, getUserByEmail, getAdminByEmail };
